3c3b243814c484077786be552f16ca50e57aaa8f
Martin Rada <martinradadl@gmail.com>
2023-02-02 18:28:40 -0500

    Fixed home footer alignment and padding

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import '../styles/footer.css'
+import React from "react";
+import "../styles/footer.css";
 
 const Footer = () => {
-    return <footer>
-    <p>IG: @manillas </p>
-    <p>manillas@example.com</p>
-    <p>1234567890</p>
-</footer>
-}
+  return (
+      <footer>
+        <p>IG: @manillas </p>
+        <p>manillas@example.com</p>
+        <p>1234567890</p>
+      </footer>
+  );
+};
 
-export {Footer}
+export { Footer };
